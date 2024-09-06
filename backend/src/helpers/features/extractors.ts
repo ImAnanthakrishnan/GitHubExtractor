083a2c403b81ef4894ee21889a2706ee158ea27a
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-09-06 13:22:03 +0530

    details extractor and saver helper. For an abstraction

---
+++ backend/src/helpers/features/extractors.ts
@@ -0,0 +1,147 @@
+import axios from "axios";
+import User from "../../models/userModel.js";
+import Repos from "../../models/reposModel.js";
+import mongoose,{ObjectId} from "mongoose";
+import Follow from "../../models/followModel.js";
+
+export const saveUserData = async (username: string) => {
+  try {
+    let user;
+    const { data } = await axios.get(`${process.env.GIT_API}/${username}`);
+
+    user = new User({
+      username: data.login,
+      location: data.location,
+      avatar:data.avatar_url,
+      bio: data.bio,
+      blog: data.blog,
+      public_repos: data.public_repos,
+      public_gists: data.public_gists,
+      followers: data.followers,
+      following: data.following,
+      created_at: new Date(data.created_at),
+      isDeleted: false,
+    });
+
+    const respoIds: ObjectId[] = await saveRepos(
+      username,
+      user._id
+    ); //saving repo's
+
+    const {followerIds,followingIds,friends} = await saveFollowDetails(username,user._id); //saved and extracted id
+
+    //all id's updated;
+    user.repos = respoIds;
+    user.follow_details.followers = followerIds;
+    user.follow_details.following = followingIds;
+    user.friends = friends; //friends added
+   
+    await user.save(); //saved entire data;
+
+    return { message: 'User data fetched, saved, and updated successfully', user };
+  } catch (error: any) {
+    console.log(error.message);
+    throw new Error("Failed to process :" + error.message);
+  }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////
+const saveRepos = async (username: string, userId: unknown) => {
+  try {
+    const { data } = await axios.get(
+      `${process.env.GIT_API}/${username}/repos`
+    );
+
+    const repoIds: ObjectId[] = await Promise.all(
+      data.map(async (repoData: any) => {
+        let repo = await Repos.findOne({ repoName: repoData, userId }); //checking for duplicate repo in same username
+        if (!repo) {
+          repo = new Repos({
+            userId,
+            repoName: repoData.name,
+            description: repoData.description || "",
+            topics: repoData.topics || [],
+          });
+          await repo.save();
+        }
+        return repo._id;
+      })
+    ); // for parallel execution big data can be extracted so need to be effectively updated by properly waiting.
+
+    return repoIds;
+  } catch (error: any) {
+    console.log(error.message);
+    throw new Error("Failed to process :" + error.message);
+  }
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+const saveFollowDetails = async (username: string, userId: unknown) => {
+  try {
+    const [followers, following] = await Promise.all([
+      axios.get(`${process.env.GIT_API}/${username}/followers`),
+      axios.get(`${process.env.GIT_API}/${username}/following`),
+    ]);
+
+    let followDocument = await Follow.findOne({ userId });
+
+    if (!followDocument) {
+      // If no document exists for this userId,
+      followDocument = new Follow({
+        userId: userId,
+        followers: [],
+        following: [],
+      });
+    }
+
+    //follower data save and extract id;
+    const followerIds: ObjectId[] = await Promise.all(
+      followers.data.map(async (followerData: any) => {
+        const existingFollower = followDocument.followers.find(
+          (f) => f.name === followerData.login
+        );
+
+        if (!existingFollower) {
+          followDocument.followers.push({ name: followerData.login,avatar:followerData.avatar_url });
+        }
+
+        return followerData._id;
+      })
+    );
+
+    //follower data save and extract id;
+    const followingIds: ObjectId[] = await Promise.all(
+      following.data.map(async (followingData: any) => {
+        const existingFollowing = followDocument.following.find(
+          (f) => f.name === followingData.login
+        );
+
+        if (!existingFollowing) {
+          followDocument.followers.push({ name: followingData.login,avatar:followingData.avatar_url });
+        }
+
+        return followingData._id;
+      })
+    );
+
+    await followDocument.save(); //saved all data;
+
+    //find friends-------
+    const friends = await findFriends(followers.data,following.data);
+
+    return { followerIds, followingIds,friends };
+  } catch (error:any) {
+    console.log(error.message);
+    throw new Error("Failed to process :" + error.message);
+  }
+};
+
+const findFriends = async(followers:{name:string,avatar:string}[],followings:{name:string,avatar:string}[]) => {
+  //lookup for following 
+  const followingSet = new Set(followings.map(user => user.name));
+
+  // Filter followers to find mutual
+  const mutualFriends = followers
+    .filter(follower => followingSet.has(follower.name))
+    .map(f => ({ name: f.name, avatar: f.avatar }));
+
+  return mutualFriends;
+}
