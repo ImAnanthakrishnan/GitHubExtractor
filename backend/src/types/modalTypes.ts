2134b7303b20f9c9e6e129af398c7439eb529211
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-09-07 17:16:12 +0530

    code arranged

--- backend/src/types/modalTypes.ts
+++ backend/src/types/modalTypes.ts
@@ -1,9 +1,9 @@
-import { Document,ObjectId } from 'mongoose';
+import { Document, ObjectId } from "mongoose";
 
 export interface IUser extends Document {
   username: string;
   location?: string;
-  avatar:string;
+  avatar: string;
   bio?: string;
   blog?: string;
   public_repos: number;
@@ -17,28 +17,28 @@ export interface IUser extends Document {
   };
   friends: {
     name: string;
-    avatar:string;
+    avatar: string;
   }[];
-  repos: ObjectId[];  
+  repos: ObjectId[];
   isDeleted: boolean;
 }
 //////////////////////////////////////////////////////
 interface IFollower {
   name: string;
-  avatar:string;
-  id:ObjectId;
+  avatar: string;
+  id: ObjectId;
 }
 
 export interface IFollow extends Document {
-  userId:ObjectId;
+  userId: ObjectId;
   followers: IFollower[];
   following: IFollower[];
 }
 ////////////////////////////////////////////////////////////
 
 export interface IRepo {
-  userId:ObjectId;
+  userId: ObjectId;
   repoName: string;
   description: string;
-  topics?: string[]; 
+  topics?: string[];
 }
