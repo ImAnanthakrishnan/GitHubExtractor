5d1b1e9db2262d06d619118f868fcbb70be1308e
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-09-07 09:12:55 +0530

    atlas connection

---
+++ backend/src/services/dbConnection.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+class DatabaseConnection {
+    uri: string;
+    constructor(uri: string | undefined) {
+        if (!uri) {
+          throw new Error("Database URI is not provided");
+        }
+        this.uri = uri;
+      }
+  
+    async connect() {
+      try {
+        const connection = await mongoose.connect(this.uri);
+        console.log("Connected to DB.");
+      } catch (error:any) {
+        console.log("Error connecting to DB:", error.message);
+      }
+    }
+}
+
+  export default DatabaseConnection
